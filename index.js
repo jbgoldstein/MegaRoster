9c200b0ea85cf9834d9eff39a31a56412d25da65
Jack Goldstein <superuserperson4@gmail.com>
2017-05-17 09:43:54 -0400

    Make submit button add names to the roster

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 {
     const personForm = document.querySelector('form')
 
+    const renderItem = (name) => {
+        console.log(name)
+        const item = document.createElement('li')
+        item.textContent = name
+        return item
+    }
+
     const handleSubmit = (ev) => {
         ev.preventDefault()
         const form = ev.target
         const roster = document.querySelector('.roster')
 
-        
+        const nameItem = renderItem(form.personName.value)
+        roster.appendChild(nameItem)
     }
 
     personForm.addEventListener('submit', handleSubmit)
